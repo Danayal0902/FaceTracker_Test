e6d1ebd910c713a34cdb1684208eeedf41ba6288
Danayal Iftikhar <danayal_94@hotmail.com>
2018-04-11 01:42:51 +0100

    Added live preview with tracking

--- FaceTrackerTest/app/src/main/java/com/example/danayaliftikhar/facetrackertest/MainActivity.java
+++ FaceTrackerTest/app/src/main/java/com/example/danayaliftikhar/facetrackertest/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.danayaliftikhar.facetrackertest;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +20,31 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import java.io.IOException;
+
+//main activity for the face tracker app
+public final class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "FaceTracker";
+
+    private CameraSourcePreview mPreview;
+    private GraphicOverlay mGraphicOverlay;
+
+    private CameraSource mCameraSource = null;
+
+
+
+    private static final int RC_HANDLE_GMS = 9001;
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
 
     private TextView mTextMessage;
 
@@ -37,30 +68,66 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    public MainActivity() {
+    }
+
+    //creates the UI and starts the live preview
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mPreview = findViewById(R.id.preview);
+        mGraphicOverlay = findViewById(R.id.faceOverlay);
+
+        // Check for the camera permission before accessing the camera.  If the
+        // permission is not granted yet, request permission.
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        }
+        else
+        {
+            requestCameraPermission();
+        }
+
+
+
+        //code for bottom nav bar (this was not used)
         mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        Button b = findViewById(R.id.btnStart);
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .build();
 
-        b.setOnClickListener(new View.OnClickListener() {
+        detector.setProcessor(new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory()).build());
+
+
+        //create button to start the camera
+        Button start = findViewById(R.id.btnStart);
+
+        start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.VIDEO_CAPTURE");
-                startActivity(intent);
+
+                //starts an intent to allow the camera to capture video
+                Intent video = new Intent("android.media.action.VIDEO_CAPTURE");
+                startActivity(video);
+
+
             }
         });
 
+        //used to enable face outline in the camera for any detected faces in frame
         CheckBox outline = findViewById(R.id.chkbxOutline);
         outline.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
                 if (isChecked) {
+
+                    //generates a popup to notify the user
                     Toast.makeText(getApplicationContext(), "Object outline is active.", Toast.LENGTH_LONG).show();
                 }
                 else
@@ -69,6 +136,181 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    /**
+     * Handles the requesting of the camera permission.  This includes
+     * showing a "Snackbar" message of why the permission is needed then
+     * sending the request.
+     */
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        //creates the dialog to ask user for camera permissions
+        Snackbar.make(mGraphicOverlay, R.string.app_name,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.app_name, listener)
+                .show();
+    }
+
+
+
+    //creates and starts the live preview on screen
+    private void createCameraSource() {
+
+
+        //properties for the face detector and tracker
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        detector.setProcessor(
+                new MultiProcessor.Builder<>(new GraphicFaceTrackerFactory())
+                        .build());
+
+        if (!detector.isOperational()) {
+            // Note: The first time that an app using face API is installed on a device, GMS will
+            // download a native library to the device in order to do detection.  Usually this
+            // completes before the app is run for the first time.  But if that download has not yet
+            // completed, then the above call will not detect any faces.
+            //
+            // isOperational() can be used to check if the required native library is currently
+            // available.  The detector will automatically become operational once the library
+            // download completes on device.
+            Log.w(TAG, "Face detector dependencies are not yet available.");
+        }
+
+        //display properties for the live preview
+        mCameraSource = new CameraSource.Builder(getApplicationContext(), detector)
+                .setRequestedPreviewSize(640, 480)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(30.0f)
+                .build();
+
+
+    }
+
+    /**
+     * Restarts the camera.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCameraSource();
+    }
+
+    /**
+     * Stops the camera.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreview.stop();
+    }
+
+    /**
+     * Releases the resources associated with the camera source, the associated detector, and the
+     * rest of the processing pipeline.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCameraSource != null) {
+            mCameraSource.release();
+        }
+    }
+
+
+    //starts the face detection preview
+    private void startCameraSource() {
+
+        //check that the device has Google Play services available.
+        int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
+                getApplicationContext());
+        if (code != ConnectionResult.SUCCESS) {
+            Dialog dlg =
+                    GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
+            dlg.show();
+        }
+
+        //throws an error message if there is no camera source or permissions have not been given
+        if (mCameraSource != null) {
+            try {
+                mPreview.start(mCameraSource, mGraphicOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                mCameraSource.release();
+                mCameraSource = null;
+            }
+        }
+    }
+
+    //factory pattern to create the face tracker. A new outline will be created for each face detected
+    private class GraphicFaceTrackerFactory
+            implements MultiProcessor.Factory<Face> {
+        @Override
+        public Tracker<Face> create(Face face) {
+            return new GraphicFaceTracker(mGraphicOverlay);
+        }
+    }
+
+    //a class which creates an outline for each face in the frame,
+    // assigning individual faces different colours
+    private class GraphicFaceTracker extends Tracker<Face> {
+        private GraphicOverlay mOverlay;
+        private FaceGraphic mFaceGraphic;
+
+        GraphicFaceTracker(GraphicOverlay overlay) {
+            mOverlay = overlay;
+            mFaceGraphic = new FaceGraphic(overlay);
+        }
+
+        //starts tracking a new face in the frame
+        @Override
+        public void onNewItem(int faceId, Face item) {
+            mFaceGraphic.setId(faceId);
+        }
+
+        //updates the position of faces on each frame
+        @Override
+        public void onUpdate(FaceDetector.Detections<Face> detectionResults,
+                             Face face) {
+            mOverlay.add(mFaceGraphic);
+            mFaceGraphic.updateFace(face);
+        }
+
+        //the face outline shape will disappear when the corresponding
+        //image or face leaves the screen
+        @Override
+        public void onMissing(FaceDetector.Detections<Face> detectionResults) {
+            mOverlay.remove(mFaceGraphic);
+        }
+
+        //does the same as above but removes the assigned colour for a face if it is gone
+        @Override
+        public void onDone() {
+            mOverlay.remove(mFaceGraphic);
+        }
     }
 
 }
